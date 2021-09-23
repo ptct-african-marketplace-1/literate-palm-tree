21e386acb9ec9f0c4bf8d1901a98e3f539a5d043
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-09-23 01:26:14 -0500

    tried adding background img, i gave up, lol im super sleepy

--- front-end/african-marketplace/src/components/About.js
+++ front-end/african-marketplace/src/components/About.js
@@ -3,9 +3,9 @@ import React from 'react'
 const About = () => {
 	return (
 		<h2> huge thanks to our team,
-			<h3>Jake Linn</h3>
-			<h3>Juan Ordonez</h3>
-			<h3>Steve Rivera</h3>
+			<p>Jake Linn</p>
+			<p>Juan Ordonez</p>
+			<p>Steve Rivera</p>
 		</h2>
 	)
 }
--- front-end/african-marketplace/src/components/Navbar.js
+++ front-end/african-marketplace/src/components/Navbar.js
@@ -8,8 +8,8 @@ const NavBar = () => {
 		<div className="Navbar">
 			<Link to="/">African Marketplace</Link>
 			<Link to="/login">Log In</Link>
-			<Link to="/about">About</Link>
 			<Link to="/register">Create Acount</Link>	
+			<Link to="/about">About</Link>
 		</div>
         
 	)
